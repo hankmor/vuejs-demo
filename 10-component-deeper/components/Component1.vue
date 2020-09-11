36750d1f60df97f823b7f2c637676c61bf8de8d0
sam <belonk@126.com>
2020-09-11 16:57:48 +0800

    add component-deeper demo

---
+++ 10-component-deeper/components/Component1.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<script>
+    var component1 = {
+        props: ['content'],
+        template: `
+            <p>{{content}}</p>
+        `
+    }
+
+    export default {
+        components: {
+            component1,
+        },
+        // ...
+    }
+</script>
